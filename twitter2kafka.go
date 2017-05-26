c1e70794d2672e7693bd332725e7ee97b54c94b5
Gerard Casas Saez <casassg@gmail.com>
2017-05-26 00:17:23 -0600

    hard coded version

--- twitter2kafka.go
+++ twitter2kafka.go
@@ -2,12 +2,34 @@ package main
 
 // OAuth1
 import (
-	"github.com/ChimeraCoder/anaconda"
+	"github.com/dghubble/oauth1"
 	"os"
-	//"net/url"
-	"net/url"
-	"encoding/json"
+	"bufio"
+	"strings"
 )
+
+func scanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := strings.Index(string(data), "\r\n"); i >= 0 {
+		// We have a full '\r\n' terminated line.
+		return i + 2, data[0:i], nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), dropCR(data), nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
+
+func dropCR(data []byte) []byte {
+	if len(data) > 0 && data[len(data)-1] == '\n' {
+		return data[0: len(data)-1]
+	}
+	return data
+}
 
 func main() {
 	var access_token = os.Getenv("ACCESS_TOKEN")
@@ -16,20 +38,27 @@ func main() {
 	var consumer_secret = os.Getenv("CONSUMER_SECRET")
 	//var tokens = os.Getenv("TOKENS")
 
-	anaconda.SetConsumerKey(consumer_key)
-	anaconda.SetConsumerSecret(consumer_secret)
-	client := anaconda.NewTwitterApi(access_token, token_secret)
+	conf := oauth1.NewConfig(consumer_key, consumer_secret)
+	token := oauth1.NewToken(access_token, token_secret)
+	client := conf.Client(oauth1.NoContext, token)
+	stream_url := "https://stream.twitter.com/1.1/statuses/filter.json?track=Trump"
 
-	values := url.Values{}
-	values.Set("track", "Trump")
-	stream := client.PublicStreamFilter(values)
+	resp, _ := client.Post(stream_url, "application/json", nil)
 
-	//stream := client.PublicStreamSample(nil)
+	scanner := bufio.NewScanner(resp.Body)
+	scanner.Split(scanLines)
 
-	for {
-		item := <-stream.C
-		jsonTweet, _ := json.Marshal(item)
-		println(string(jsonTweet))
+	count := 0
+	for scanner.Scan() {
+		token := scanner.Bytes()
+		if len(token) == 0 {
+			// empty keep-alive
+			continue
+		}
+		//println(string(token))
+		//println("-----------------------")
+		println(count)
+		count += 1
 
 	}
 }
